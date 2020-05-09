1dfcb9ac905e256629695b85428ad246f8052c13
Ricardo Antonio López Castillo <rychy126@gmail.com>
2020-05-08 19:58:43 -0600

    mas lineas

--- sketch.js
+++ sketch.js
@@ -1,13 +1,27 @@
-const p1 = {x:0, y:0 }
-const p2 = {x:windowWidth, y:300}
+//const p1 = {x:0, y:0 }
+//const p2 = {x:800, y:300}
 
 function setup(){
     createCanvas(windowWidth, windowHeight)
+    p1 = {x:0, y:0}
+    p2 = {x:floor(windowWidth), y:floor(windowHeight)}
+
+    p3 = {x:0, y:floor(windowHeight/2)}
+    p4 = {x:floor(windowWidth), y:floor(windowHeight/2)}
+
+    p5 = {x:0, y:floor(windowHeight)}
+    p6 = {x:floor(windowWidth), y:0}
+
+    p7 = {x:floor(windowWidth/2), y:0}
+    p8 = {x:floor(windowWidth/2), y:floor(windowHeight)}
 }
 
 function draw(){
     //line(p1.x,p1.y,p2.x,p2.y)
     ecuapp(p1,p2)
+    ecuapp(p3,p4)
+    ecuapp(p5,p6)
+    ecuapp(p7,p8)
 }
 
 function ecuapp(p1,p2){
